38213dabcb3be307d3502480fd49895710eea1b0
LIGECT <viminiko@gmail.com>
2025-07-06 20:48:39 +0300

    Create a div attribute by iterating over the array for each book, add the class and ID

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
 const myLibrary = [];
+const booksArea = document.getElementById("books-display-area");
 
 function Book(title, author, pages, read) {
-  if (!new.target) {
-    throw Error("You must use the 'new' operator to call the constructor");
-  }
-
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
   this.id = crypto.randomUUID();
-
-  Book.prototype.info = function () {
-    const readStatus = this.read ? "read" : "not read";
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
-  };
 }
 
+Book.prototype.info = function () {
+  const readStatus = this.read ? "read" : "not read";
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
+};
+
 function addBookLibrary(title, author, pages, read) {
-  // take params, create a book then store it in the array
   let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
 }
 
 addBookLibrary("The Art of Not Giving a Fuck", "Mark Manson", 224, true);
-addBookLibrary("JavaScript for Sadists", "Linus Devberg", 666, false);
-addBookLibrary("Eat. Sleep. Code. Repeat.", "Ada Lovelace", 342, true);
-addBookLibrary("The Subtle Joy of Syntax Errors", "Chuck Brackett", 129, false);
-addBookLibrary("101 Ways to Rage Quit", "Devon McCrash", 403, true);
-// addBookLibrary("Midnight Debugging", "Sarah Nullpointer", 250, false);
-// addBookLibrary("Gods of Stack Overflow", "Johnny Hotfix", 500, true);
-// addBookLibrary("Coffee & Commit Messages", "Tina Gitpush", 312, true);
-// addBookLibrary("Why My Code Works (I Don't Know)", "Random Dev", 420, false);
-// addBookLibrary("Fuck You, I'm Refactoring", "Karen Semicolon", 350, true);
+// addBookLibrary("JavaScript for Sadists", "Linus Devberg", 666, false);
+// addBookLibrary("Eat. Sleep. Code. Repeat.", "Ada Lovelace", 342, true);
 
-console.log(myLibrary);
+myLibrary.forEach((book) => {
+  const myDiv = document.createElement("div");
+  const bookID = book.id;
+  myDiv.setAttribute("data-book-id", bookID);
+  booksArea.dataset.bookId;
+  myDiv.classList.add("book-card");
+
+  //   element.textContent = book.title;
+  booksArea.appendChild(myDiv);
+});
